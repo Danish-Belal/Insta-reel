0d366de01a5b5c076d9b48b0c9a40b597b766065
Danish-Belal <danishexplore019@gmail.com>
2022-11-30 23:19:59 +0530

    :fire: auth

---
+++ instagram/context/auth.js
@@ -0,0 +1,105 @@
+// import React, { createContext, useEffect, useState } from "react";
+// import { auth } from "../firebase";
+// export const AuthContext = createContext();
+// import {
+//   signInWithEmailAndPassword,
+//   onAuthStateChanged,
+//   signOut,
+// } from "firebase/auth";
+// function AuthWrapper({ children }) {
+//   console.log("This is Authorization page");
+//   const [user, setUser] = useState("");
+//   const [loding, setLoading] = useState(true);
+//   useEffect(() => {
+//     onAuthStateChanged(auth, (user) => {
+//       if (user) {
+//         // User is signed in, see docs for a list of available properties
+//         setUser(user);
+//         const uid = user.uid;
+//         // ...
+//       } else {
+//         // User is signed out
+//         // ...
+//       }
+//     },[]);
+//     setLoading(false);
+//   }, []);
+//   function login(email, password) {
+//     return signInWithEmailAndPassword(auth, email, password);
+//   }
+
+//   function logout() {
+//     return signOut(auth);
+//   }
+//   const store = {
+//     login,
+//     logout,
+//     user
+//   }
+//   return (
+//     <AuthContext.Provider value={store}>
+//       {!loding && {children}}
+//     </AuthContext.Provider>
+//   );
+// }
+
+// export default AuthWrapper;
+
+import React, { createContext, useEffect, useState } from "react";
+import { auth } from "../firebase";
+export const AuthContext = createContext();
+import {
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  sendPasswordResetEmail,
+  createUserWithEmailAndPassword ,
+} from "firebase/auth";
+function AuthWrapper({ children }) {
+  console.log("hello in AuthWrapper.");
+  const [user, setUser] = useState("");
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      // if (user) {
+        // User is signed in, see docs for a list of available properties
+        setUser(user);
+        // const uid = user.uid;
+        // ...
+      // } else {
+      //   // User is signed out
+      //   // ...
+      // }
+    })
+    setLoading(false);
+  }, []);
+
+  function login(email, password) {
+    return signInWithEmailAndPassword(auth, email, password);
+  }
+
+  function logout() {
+    return signOut(auth);
+  }
+
+  function forgetPassword(email){
+    return sendPasswordResetEmail(auth , email);
+  }
+  function signup(email , password){
+    return createUserWithEmailAndPassword(auth , email , password);
+  }
+  const store = {
+    login,
+    logout,
+    user,
+    forgetPassword,
+    signup,
+  }
+  return (
+    <AuthContext.Provider value={store}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+}
+
+export default AuthWrapper;
