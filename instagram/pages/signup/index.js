30d6e4e60e0b95d2ab5118373a986036d67689d5
Danish-Belal <danishexplore019@gmail.com>
2023-01-07 21:09:50 +0530

    connected signup page to firebase storege

--- instagram/pages/signup/index.js
+++ instagram/pages/signup/index.js
@@ -1,5 +1,4 @@
 import Image from "next/image";
-
 import logo from "../../assets/logo.png";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
@@ -9,8 +8,11 @@ import Link from "next/link";
 import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../context/auth";
 import { Route } from "@mui/icons-material";
+import { getStorage,getDownloadURL, ref, uploadBytesResumable  } from "firebase/storage";
+import { storage, Storage } from "../../firebase";
 
 function index() {
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [fullName, setFullName] = useState("");
@@ -29,9 +31,58 @@ function index() {
       setLoading(true);
       setError("");
       let userInfo = await signup(email, password);
-      console.log(userInfo);
-      console.log("User signup");
+      console.log(JSON.stringify(userInfo));
+      // console.log("User signup");
       // router.push("/");
+
+        // Upload file and metadata to the object 'images/mountains.jpg'
+      const storageRef = ref(storage , `${userInfo.user.uid}/Profile`);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      // Listen for state changes, errors, and completion of the upload.
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+          // switch (snapshot.state) {
+          //   case "paused":
+          //     console.log("Upload is paused");
+          //     break;
+          //   case "running":
+          //     console.log("Upload is running");
+          //     break;
+          // }
+        },
+        (error) => {
+          // A full list of error codes is available at
+          // https://firebase.google.com/docs/storage/web/handle-errors
+          // switch (error.code) {
+          //   case "storage/unauthorized":
+          //     // User doesn't have permission to access the object
+          //     break;
+          //   case "storage/canceled":
+          //     // User canceled the upload
+          //     break;
+
+          //   // ...
+
+          //   case "storage/unknown":
+          //     // Unknown error occurred, inspect error.serverResponse
+          //     break;
+          // }
+          console.log(error);
+        },
+        () => {
+          // Upload completed successfully, now we can get the download URL
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            console.log("File available at", downloadURL);
+          });
+        }
+      );
+      console.log("User Signed in")
     } catch (err) {
       console.log("err", JSON.stringify(err));
       setError(err.code);
