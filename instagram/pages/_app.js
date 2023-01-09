66d4df35de960397292e95de2dbef0d62fc13be3
Danish-Belal <danishexplore019@gmail.com>
2023-01-09 23:02:07 +0530

    App.js file

--- instagram/pages/_app.js
+++ instagram/pages/_app.js
@@ -4,6 +4,7 @@ import "./login/login.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "../components/Feed.css";
 import AuthWrapper from "../context/auth";
+import '../components/Profile.css'
 
 function MyApp({ Component, pageProps }) {
   return(
