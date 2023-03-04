5fc908e219e2b1a16420bd33e7b131b4bb5eca99
Danish-Belal <danishexplore019@gmail.com>
2023-03-04 22:49:41 +0530

    🚧 ProtectRoute

--- instagram/pages/profile.js
+++ instagram/pages/profile.js
@@ -1,11 +1,15 @@
-import React from 'react'
-
-
-import Profile from '../components/Profile'
-
+import React, { useContext } from "react";
+import { useRouter } from "next/router";
+import Profile from "../components/Profile";
+import { AuthContext } from "../context/auth";
 
 export default function profile() {
-  return (
-    <h1><Profile /></h1>
-  )
+  const { user } = useContext(AuthContext);
+
+  const Redirect = () => {
+    const router = useRouter();
+    router.push("/login");
+  };
+
+  return <>{user?.uid ? <Profile /> : <Redirect />}</>;
 }
